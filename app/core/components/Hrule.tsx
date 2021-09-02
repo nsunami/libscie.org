35d13d7cf38b36a1d0a1df1e90b60c2e75a0cc49
Chris Hartgerink <chris@libscie.org>
2021-09-02 11:08:41 +0200

    Add Hrule

---
+++ app/core/components/Hrule.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Hrule() {
+  return (
+    <hr className="w-full mt-4 mb-4 border-t-0 bg-gradient-to-r from-pink-400 to-indigo-600 bg-yellow-400 h-0.5 " />
+  )
+}
