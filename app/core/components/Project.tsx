a67e257e0710592234fdf1ccf04b5ea810096a31
Chris Hartgerink <chris@libscie.org>
2021-09-02 11:08:57 +0200

    Add Project

---
+++ app/core/components/Project.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { ChevronDown24 } from "@carbon/icons-react"
+import { Link } from "blitz"
+
+export default function Project({ title, description }) {
+  return (
+    <div className="sm:flex mt-8 mb-8 dark:text-white">
+      <div className="sm:w-52">
+        <ChevronDown24 className="w-52 h-52 mx-auto" />
+      </div>
+      <div className="w-full pl-8">
+        <h2 className="text-2xl mb-6">{title}</h2>
+        <p className="text-base mb-6">{description}</p>
+        <Link href="#">
+          <a className="text-base border-2 border-indigo-600 pt-1 pb-1 pl-4 pr-4 mr-4 hover:bg-indigo-600 hover:text-white">
+            Learn more
+          </a>
+        </Link>
+        <Link href="#">
+          <a className="text-base border-2 border-indigo-600 pt-1 pb-1 pl-4 pr-4 hover:bg-indigo-600 hover:text-white">
+            Learn more
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
