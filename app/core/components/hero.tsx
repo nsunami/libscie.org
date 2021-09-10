00731524a27a135b2fe96a094a0170f9b7b2f088
Chris Hartgerink <chris@libscie.org>
2021-09-10 11:38:55 +0200

    Fix Hero sizing for mobile

--- app/core/components/hero.tsx
+++ app/core/components/hero.tsx
@@ -4,7 +4,7 @@ import { Link } from "blitz"
 export default function Hero() {
   return (
     <div className="max-w-7xl z-40 mx-auto px-4 sm:px-6">
-      <h1 className="pt-20 pb-5 text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-indigo-600 h-auto">
+      <h1 className="pt-20 pb-5 text-5xl sm:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-indigo-600 h-auto">
         Your partner in repairing knowledge distribution
       </h1>
       <p className="max-w-xl text-black dark:text-white mb-5 font-bold">
